7b25ae8c8c64a941e0d77f3e5c2b2052a2ce6443
roshitha2707 <72878563+roshita2707@users.noreply.github.com>
2022-01-05 11:26:58 +0530

    Add files via upload

---
+++ Skill.java
@@ -0,0 +1,21 @@
+package fasterxml;
+
+public class Skill {
+	String skillName;
+
+	public String getSkillName() {
+		return skillName;
+	}
+
+	public void setSkillName(String skillName) {
+		this.skillName = skillName;
+	}
+	
+	public String toString() {
+		return this.skillName;
+		
+	}
+	
+	
+
+}
